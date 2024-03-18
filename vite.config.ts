4cbc8ee91c8b750e0842c9cb676a1b4586793767
Alexander Kachan <akach.dev@mail.ru>
2024-03-18 19:46:07 +0300

    fix: change homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/hacker-news',
+  base: '/hacker-news/',
   envPrefix: 'APP_',
   plugins: [react()],
   resolve: {
